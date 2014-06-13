e3452815971b63317b04932e100e8235e398ce49
Stephen Brennan <stephen.brennan@case.edu>
2014-06-13 10:19:09 -0400

    Changed whitespace and made default domain of entropy a tuple instead of a list.

--- compare.py
+++ compare.py
@@ -17,11 +17,13 @@ from pandas import Series, DataFrame
 ################################################################################
 # Mutual information / Entropy calculation
 
+
 def _term(pxy, px, py):
     if px == 0 or py == 0 or pxy == 0:
         return 0
     else:
         return pxy * np.log2(pxy / (px * py))
+
 
 def binary_mutual_information(dataSet1, dataSet2):
     """Computes the mutual information (in bits) of two binary datasets.
@@ -51,17 +53,19 @@ def binary_mutual_information(dataSet1, dataSet2):
                          + _term(px0y0, px0, py0)
     return mutual_information
 
-def entropy(dataSet, domain = [0, 1]):
+
+def entropy(dataSet, domain=(0, 1)):
     """Computes the entropy (in bits) of a dataset."""
     entropy = 0
     total = len(dataSet)
     for value in domain:
         probability = len(dataSet[dataSet == value]) / total
-        if probability != 0: # avoid log(0)
+        if probability != 0:  # avoid log(0)
             entropy -= probability * np.log2(probability)
     return entropy
 
-def conditional_entropy(dataSet, conditionSet, dsDomain=[0,1], csDomain=[0,1]):
+
+def conditional_entropy(dataSet, conditionSet, dsDomain=[0, 1], csDomain=[0, 1]):
     """Computes the conditional entropy of dataSet given conditionSet."""
     entropy = 0
     total = len(dataSet)
@@ -69,9 +73,10 @@ def conditional_entropy(dataSet, conditionSet, dsDomain=[0,1], csDomain=[0,1]):
         for c in csDomain:
             pBoth = len(dataSet[(dataSet == d) & (conditionSet == c)]) / total
             pCondition = len(conditionSet[conditionSet == c]) / total
-            if pCondition != 0: # if pCondition == 0, pBoth == 0 too
+            if pCondition != 0:  # if pCondition == 0, pBoth == 0 too
                 entropy += pBoth * np.log2(pCondition / pBoth)
     return entropy
+
 
 def compare_mi_methods(dataSet1, dataSet2):
     """Computes binary mutual information multiple ways and compares.
@@ -87,23 +92,24 @@ def compare_mi_methods(dataSet1, dataSet2):
     combined = dataSet1 + dataSet2 * 2
     results = [
         binary_mutual_information(dataSet1, dataSet2),
-        
+
         entropy(dataSet1) - conditional_entropy(dataSet1, dataSet2),
-        
+
         entropy(dataSet2) - conditional_entropy(dataSet2, dataSet1),
 
-        entropy(dataSet1) + entropy(dataSet2) - entropy(combined, 
-                                                        domain=[0,1,2,3]),
+        entropy(dataSet1) + entropy(dataSet2) - entropy(combined,
+                                                        domain=[0, 1, 2, 3]),
 
-        entropy(combined, domain=[0,1,2,3])           \
-            - conditional_entropy(dataSet1, dataSet2) \
-            - conditional_entropy(dataSet2, dataSet1)
+        entropy(combined, domain=[0, 1, 2, 3]) \
+        - conditional_entropy(dataSet1, dataSet2) \
+        - conditional_entropy(dataSet2, dataSet1)
     ]
     for r1 in results:
         for r2 in results:
             if not np.isclose([r1], [r2]):
                 return False
     return True
+
 
 ################################################################################
 # Boolean functions.  All are designed to take and receive bools, not integers
@@ -114,27 +120,33 @@ def dsAnd(x, y):
     """Dataset AND.  For use with Pandas Series."""
     return x.multiply(y).astype(bool)
 
+
 # ~ 192 microseconds (!)
 def dsOr(x, y):
     """Dataset OR.  For use with Pandas Series."""
     return ~(~x).multiply(~y)
+
 
 # ~ 59 microseconds
 def dsXor(x, y):
     """Dataset XOR.  For use with Pandas Series."""
     return x != y
 
+
 # ~ 99 + 46 = 145 microseconds
 def dsAndNot(x, y):
     """Dataset x AND NOT y.  For use with Pandas Series."""
     return dsAnd(x, ~y)
+
 
 # ~ 99 + 46 = 145 microseconds
 def dsNotAnd(x, y):
     """Dataset NOT x AND y.  For use with Pandas Series."""
     return dsAnd(~x, y)
 
+
 COMBINATIONS = [dsAnd, dsOr, dsXor, dsAndNot, dsNotAnd]
+
 
 def best_combination(d1, d2, p):
     """Find the best boolean combination of d1 and d2 to relate to p.
@@ -163,6 +175,7 @@ def best_combination(d1, d2, p):
             best_dataset = dataset
     return best_func, best_dataset, best_mutual_info, second_best_mutual_info
 
+
 ################################################################################
 # Benchmarks for pattern detection
 
@@ -184,6 +197,7 @@ def reclaim_pattern(size, function, proportion):
     res_func, *etc = best_combination(d1, d2, p)
     return res_func == function
 
+
 def pattern_detection_rate(size, function, trials, start=0.3, step=0.01):
     """Finds the lowest proportion at which best_combination works.
 
@@ -204,6 +218,7 @@ def pattern_detection_rate(size, function, trials, start=0.3, step=0.01):
             curr -= step
     return curr
 
+
 def all_detection_rates(size, trials=5, start=0.3, step=0.01, out=False):
     """Tests the detection rate for all functions."""
     rates = []
@@ -213,3 +228,4 @@ def all_detection_rates(size, trials=5, start=0.3, step=0.01, out=False):
             print('%s  %f' % (function.__name__, rate))
         rates.append(rate)
     return rates
+
