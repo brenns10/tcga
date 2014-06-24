10462018cd2d696f3fdcffd43d23c72a29adc408
Stephen Brennan <stephen.brennan@case.edu>
2014-06-24 13:00:59 -0400

    Added default file locations for the data.

--- tcga/parse.py
+++ tcga/parse.py
@@ -11,9 +11,14 @@
 #-------------------------------------------------------------------------------
 
 import csv
+from os.path import expanduser, expandvars
 
 import numpy as np
 import pandas as pd
+
+
+MUTSLOC = '~/data/tcga-brca/mutations.csv'
+PHENLOC = '~/data/tcga-brca/vital-status.csv'
 
 
 def _read_csv(filename):
@@ -24,13 +29,14 @@ def _read_csv(filename):
         yield from r
 
 
-def mutations(filename):
+def mutations(filename=MUTSLOC):
     """Reads a sparse mutation file.  Returns a dense Pandas DataFrame.
 
     The returned DataFrame is indexed by the patients (that is, patients are the
     row), and the columns are the genes.
 
     """
+    filename = expanduser(expandvars(filename))
     raw_muts = list(_read_csv(filename))
     genes, patients = zip(*raw_muts)  # unzip the zipped lists
     genes = set(genes)
@@ -42,8 +48,9 @@ def mutations(filename):
     return muts
 
 
-def phenotypes(filename, dtype=bool):
+def phenotypes(filename=PHENLOC, dtype=bool):
     """Reads a mutation CSV.  Returns a pd.Series indexed by patient."""
+    filename = expanduser(expandvars(filename))
     val = pd.read_csv(filename, index_col=0, squeeze=True)
     if dtype:
         return val.astype(dtype)
@@ -51,8 +58,9 @@ def phenotypes(filename, dtype=bool):
         return val
 
 
-def restrict(muts, phen):
-    """Restrict the patients to only those contained in both datasets.
+def add_empties(muts, phen):
+    """
+    Add the patients with no mutations into the mutations dataset.
 
     Unfortunately, this copies the data in memory (I'm not sure why).  Returns a
     2-tuple:
@@ -61,19 +69,19 @@ def restrict(muts, phen):
     [1]: modified phenotypes dataframe
 
     """
-    muts = muts.reindex(index=muts.index.intersection(phen.index))
-    phen = phen.reindex(index=muts.index)
+    muts = muts.reindex(index=muts.index.union(phen.index), fill_value=False)
+    phen = phen.reindex(index=muts.index, fill_value=False)
     return muts, phen
 
 
-def data(mutationsloc, phenotypesloc, phenotypesdtype=bool):
+def data(mutationsloc=MUTSLOC, phenotypesloc=PHENLOC, phenotypesdtype=bool):
     """Read both data files and restrict patients to those contained in both."""
     muts = mutations(mutationsloc)
     phen = phenotypes(phenotypesloc, phenotypesdtype)
-    return restrict(muts, phen)
+    return add_empties(muts, phen)
 
 
-def sparse_mutations(filename, phenotype=None):
+def sparse_mutations(filename=MUTSLOC, phenotype=None):
     """
     Reads the sparse mutations, and returns a list of (gene, patient) tuples.
     Restricts to only the patients included in the phenotype index,
@@ -82,6 +90,7 @@ def sparse_mutations(filename, phenotype=None):
     :param phenotype: The phenotype Series.
     :return: A Python list of (gene, patient) tuples.
     """
+    filename = expanduser(expandvars(filename))
     muts = list(_read_csv(filename))
     if phenotype is not None:
         muts = [(gene, patient) for gene, patient in muts if patient in
