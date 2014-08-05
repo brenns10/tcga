51620ab7c0ce342bdd7ae1aedf35335f0b092cfd
Stephen Brennan <stephen.brennan@case.edu>
2014-08-05 17:08:18 -0400

    Spun out the machinery behind DetectionExperiment into a separate parent class, Experiment.

--- tcga/experiment.py
+++ tcga/experiment.py
@@ -15,35 +15,155 @@ import multiprocessing as mp
 import random
 import os.path
 import traceback
+import time
+from abc import ABCMeta, abstractmethod
+from collections import OrderedDict
 
 import numpy as np
 from pandas import DataFrame
 
 from . import compare
+from .util import Progress
 
 
-def _dataframe_append(dataframe, rowdict):
-    newrow = len(dataframe)
-    dataframe.loc[newrow] = 0  # init with 0's
-    for k, v in rowdict.items():
-        dataframe.loc[newrow, k] = v
-
-
-class DetectionExperiment:
+class Experiment:
     """
-    This class encapsulates an experiment to learn more about the detection
-    of various patterns implanted at various proportions into datasets of
-    various distributions.  This can be done at various 'degrees' (that is,
-    lengths of datasets).  In other words, there are many possible
-    combinations of parameters this experiment could run at.  This class
-    helps eliminate deep nested for-loops and long function chains that make
-    custimizing parameter ranges difficult or impossible.
+    Abstract Base Class for experiment execution.
 
-    It divides the experiment into 'configurations'.  A single configuration
-    is a single set of possible parameter values.  A configuration has a task
-    that should be run with those parameters (self.run_config()).
-    Configuration tasks are run with parameter values and return rows to be
-    added to the result DataFrame(s).
+    This ABC encapsulates an 'experiment', which here is a task that
+    generates data and is repeated for a large number possibilites for its
+    parameters.  This class allows executing tasks with arbitrary numbers of
+    parameters, and arbitrary value ranges for each parameter.
+
+    This class allows these tasks to be easily executed in parallel, which can
+    save time for long-running, CPU intensive tasks.  The tasks may also be
+    executed in serial.
+    """
+    __metaclass__ = ABCMeta
+
+    params = OrderedDict()
+    completed = 0
+    num_configs = 0
+    progress = None
+
+    @staticmethod
+    def _dataframe_append(dataframe, rowdict):
+        """
+        Shortcut method for appending a row to a DataFrame.
+        :param dataframe: The DataFrame to append to.
+        :param rowdict: A dictionary containing each column's value.
+        """
+        newrow = len(dataframe)
+        dataframe.loc[newrow] = 0  # init with 0's
+        for k, v in rowdict.items():
+            dataframe.loc[newrow, k] = v
+
+    @staticmethod
+    def error_callback(exception):
+        """
+        Error callback for multiprocessing.
+
+        This function 'handles' exceptions from Processes by displaying them.
+        The run_config_wrapped() function puts tracebacks into the
+        exceptions, so that printing them here is actually meaningful.
+        :param exception: The exception thrown by run_config()
+        """
+        print(exception)
+
+    @abstractmethod
+    def run_task(self, configuration):
+        """
+        This function must be the task that is run for every configuration.
+
+        This function MUST be overridden.  It should take the configuration
+        as its argument, and return whatever data will be saved.  (Its return
+        value will be passed to task_callback()).
+        :param configuration: The task configuration.
+        :return: The data to be saved.
+        """
+        pass
+
+    @abstractmethod
+    def task_callback(self, retval):
+        """
+        This function should save the data created by each task.
+
+        This function MUST be overridden.  It will be called with the return
+        value of run_task().  It should save this data in a structure (like a
+        DataFrame) that was initialized in __init__().
+        :param retval: The value returned by run_task().
+        :return: Nothing
+        """
+        pass
+
+    def initial_task_callback(self, retval):
+        """
+        Receives callbacks from multiprocessing.
+
+        This function is called by the multiprocessing module when a task is
+        completed.  It then calls the task callback provided by overriding
+        functions.
+        :param retval: Value returned by run_config().
+        :return:
+        """
+        self.completed += 1
+        print('Completed %d/%d.' % (self.completed, self.num_configs))
+        self.task_callback(retval)
+
+    def run_task_wrapped(self, configuration):
+        """
+        Provides decent error handling in the multiprocessing module.
+
+        Since the multiprocessing module doesn't really allow you to get
+        things like stack traces from exceptions, this function wraps the
+        run_config() function, and at catches all exceptions, adding a
+        traceback in text form, so that error callback function can display
+        the traceback.
+        :param configuration: Passed to run_config()
+        :return: Return from run_config()
+        """
+        try:
+            return self.run_task(configuration)
+        except Exception:
+            raise Exception("".join(traceback.format_exc()))
+
+    def run_experiment_multiprocessing(self, processes=None):
+        """
+        Runs the experiment using the multiprocessing module.
+
+        A process worker pool is created, and tasks are delegated to each
+        worker.  Since there is some process spawning overhead, as well as
+        IPC overhead, this isn't perfect.  Tasks should be slow enough that
+        the speed gains of parallelizing outweigh the overhead of spawning
+        and IPC.
+        :param processes: Number of processes to use in the pool.  Default is
+        None. If None is given, the number from multiprocessing.cpu_count()
+        is used.
+        :return: Blocks until all tasks are complete.  Returns nothing.
+        """
+        # Setup the class variables used during the experiment.
+        self.completed = 0
+
+        # Create all possible configurations (an iterator).
+        configs = it.product(*self.params.values())
+
+        # Create a multiprocessing pool and add each configuration task.
+        resultobjs = []
+        with mp.Pool(processes=processes) as pool:
+            for configuration in configs:
+                resultobjs.append(pool.apply_async(self.run_task_wrapped,
+                    (configuration,), callback=self.initial_task_callback,
+                    error_callback=self.error_callback))
+                self.num_configs += 1
+            print('Experiment: queued %d tasks.' % self.num_configs)
+            for result in resultobjs:
+                result.wait()
+            print('Experiment: completed all tasks.')
+
+
+class DetectionExperiment(Experiment):
+    """
+    Encapsulates an experiment on how detectable functions are.
 
     In the DetectionExperiment, a configuration is a value for:
     - Degree (number of items in dataset)
@@ -52,33 +172,29 @@ class DetectionExperiment:
     The configuration task attempts to reclaim an imprinted pattern
     self.TRIALS_PER_CONFIG times for each function.  It returns information
     about false positives and negatives, and about the mutual information.
-
-    Configurations are runnable in parallel with the multiprocessing module.  I
-    may add a serial running option (for some reason).  I may also spin out
-    the machinery behind the basic experiment framework into a parent class.
     """
 
     def __init__(self):
         """
         Set up a detection experiment with default values.
         """
-        self.degrees = [500, 1000, 1500]
-        self.distributions = list(np.arange(0.05, 0.51, 0.05))
-        self.proportions = list(np.arange(0.3, 0.0, -0.01))
+        self.params['Degree'] = [500, 1000, 1500]
+        self.params['Distribution'] = np.arange(0.05, 0.51, 0.05)
+        self.params['Proportions'] = np.arange(0.3, 0.0, -0.01)
         self.TRIALS_PER_CONFIG = 50
-        self.configuration_columns = ['Degree', 'Distribution', 'Proportion']
+        self.results = []
 
         # Define the column for the detection dataset.
-        detection_columns = list(self.configuration_columns)
+        detection_columns = list(self.params.keys())
         for function in compare.COMBINATIONS:
             detection_columns.append(function.__name__ + '_implant')
             detection_columns.append(function.__name__ + '_ident')
             detection_columns.append(function.__name__ + '_guess')
 
-        self.detection_results = DataFrame(columns=detection_columns)
+        self.results.append(DataFrame(columns=detection_columns))
 
         # Define the columns for the mutual information dataset.
-        mi_columns = list(self.configuration_columns)
+        mi_columns = list(self.params.keys())
         mi_columns.append('Implanted')
         mi_columns.append('Trials')
         mi_columns.append('joint_mean')
@@ -87,9 +203,15 @@ class DetectionExperiment:
             mi_columns.append(function.__name__ + '_mean')
             mi_columns.append(function.__name__ + '_std')
 
-        self.mutual_info_results = DataFrame(columns=mi_columns)
+        self.results.append(DataFrame(columns=mi_columns))
 
-    def run_trial(self, configuration, function):
+    def task_callback(self, retval):
+        for row_list, dataframe in zip(retval, self.results):
+            for row in row_list:
+                self._dataframe_append(dataframe, row)
+
+    @staticmethod
+    def run_trial(configuration, function):
         """
         Run a single trial of the configuration task, with the given function.
 
@@ -132,10 +254,10 @@ class DetectionExperiment:
         # phenotype.
         joint_distribution = ds1 + 2*ds2
         joint_mutual_info = compare.mutual_info(joint_distribution, phen,
-                                                   ds1domain=4)
+                                                ds1domain=4)
         return mutual_info, max_f, joint_mutual_info
 
-    def run_config(self, configuration):
+    def run_task(self, configuration):
         """
         Run a single configuration.
         :param configuration: The configuration parameters.
@@ -198,95 +320,18 @@ class DetectionExperiment:
             detection_row[func.__name__ + '_guess'] = guess[func]
 
         # Return our detection row and mutual information rows.
-        return detection_row, mi_rows
+        return [detection_row], mi_rows
 
-    def run_config_wrapped(self, configuration):
+    def save(self, savedir='.', filenames=None):
         """
-        Provides decent error handling in the multiprocessing module.
-
-        Since the multiprocessing module doesn't really allow you to get
-        things like stack traces from exceptions, this function wraps the
-        run_config() function, and at catches all exceptions, adding a
-        traceback in text form, so that error callback function can display
-        the traceback.
-        :param configuration: Passed to run_config()
-        :return: Return from run_config()
-        """
-        try:
-            return self.run_config(configuration)
-        except Exception:
-            raise Exception("".join(traceback.format_exc()))
-
-    def experiment_callback(self, retVal):
-        """
-        Store data produced by run_config().
-
-        This function is called by the multiprocessing module when a task is
-        completed.  It is executed on the main process, so no synchronization is
-        needed to access the DataFrame.
-        :param retVal: Value returned by run_config().
-        :return:
-        """
-        detection_row, mi_rows = retVal
-        _dataframe_append(self.detection_results, detection_row)
-        for row in mi_rows:
-            _dataframe_append(self.mutual_info_results, row)
-        self.completed += 1
-        print('Completed %d of %d.' % (self.completed, self.nconfigs))
-
-    def errcb(self, exception):
-        """
-        Error callback for multiprocessing.
-
-        This function 'handles' exceptions from Processes by displaying them.
-        The run_config_wrapped() function puts tracebacks into the
-        exceptions, so that printing them here is actually meaningful.
-        :param exception: The exception thrown by run_config()
-        :return:
-        """
-        print(exception)
-
-    def run_experiment_multiprocessing(self, processes=None):
-        """
-        Runs the experiment using the multiprocessing module.
-
-        A process worker pool is created, and tasks are delegated to each
-        worker.  Since there is some process spawning overhead, as well as
-        IPC overhead, this isn't perfect.  Tasks should be slow enough that
-        the speed gains of parallelizing outweigh the overhead of spawning
-        and IPC.
-        :param processes: Number of processes to use in the pool.  Default is
-        None. If None is given, the number from multiprocessing.cpu_count()
-        is used.
-        :return: Blocks until all tasks are complete.  Returns nothing.
-        """
-        configs = list(it.product(self.degrees, self.distributions,
-                                  self.proportions))
-        self.nconfigs = len(configs)
-        self.completed = 0
-        resultobjs = []
-
-        print('Running %d configurations.' % self.nconfigs)
-
-        with mp.Pool(processes=processes) as pool:
-            for configuration in configs:
-                resultobjs.append(pool.apply_async(self.run_config_wrapped,
-                    (configuration,), callback=self.experiment_callback,
-                    error_callback=self.errcb))
-            print('All jobs submitted.')
-            for result in resultobjs:
-                result.wait()
-
-    def save(self, savedir, detection_results_name='detection_results.pickle',
-             mutual_info_results_name='mutual_info_results.pickle'):
-        """
-        Picle the result data frames.
+        Picle the result data frames.  Stores all files in savedir.  They can be
+        given custom filenames, or saved as 'resultN.pickle'.  
         :param savedir: Directory to save in.
-        :param detection_results_name: Name for the detection results.
-        :param mutual_info_results_name: Name for the mutual info results.
+        :param filenames: File names for each result DataFrame.
         """
+        if filenames is None:
+            filenames = ['result%d.pickle' % i for i in range(len(self.results))]
+
         savedir = os.path.expandvars(os.path.expanduser(savedir))
-        self.detection_results.to_pickle(os.path.join(savedir,
-                                                      detection_results_name))
-        self.mutual_info_results.to_pickle(os.path.join(savedir,
-                                                        mutual_info_results_name))
+        for filename, result in zip(filenames, self.results):
+            result.to_pickle(os.path.join(savedir, filename))
