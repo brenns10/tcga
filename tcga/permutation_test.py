6e3e1d2574f2ab0ff603fd3cb84e4c5b693033e1
Stephen Brennan <stephen.brennan@case.edu>
2015-01-23 18:03:02 -0500

    Updated permutation test to be comprehensive.

--- tcga/permutation_test.py
+++ tcga/permutation_test.py
@@ -13,7 +13,7 @@ import random
 from .experiment import Experiment
 from tcga import compare
 from tcga.pattern import dag_pattern_recover
-from .tree import get_max_root, terminal_function_nodes
+from .tree import terminal_function_nodes
 from .compare import mutual_info
 from . import parse
 
@@ -82,102 +82,27 @@ def randomize_mutations(mutations, sparse_mutations, num_iterations=None):
 
 
 class PermutationTest(Experiment):
-    """
-    Experiment to compare pattern detection on real mutations vs random ones.
+    """Experiment to compare pattern detection on real mutations vs random ones.
 
-    This experiment does the following task many times: create a random
-    dataset similar to the real mutations dataset.  Run the pattern detection
-    procedure on the randomly generated one.  Return the mutual information
-    associated with the best pattern in the DAG.
+    This experiment does the following task many times: create a random dataset
+    similar to the real mutations dataset.  Run the pattern detection procedure
+    on the randomly generated one.  Return the comparison value of the top N
+    results.
 
     Coupled with the mutual information, this can provide evidence that a
     real pattern has been detected.
+
     """
 
     def __init__(self, phenotype='vital-status', mutations='mutations',
-                 comparison=mutual_info, trials=1000):
+                 comparison=mutual_info, trials=1000, ranks=200):
         """
         Create an instance of PermutationTest.
+        :param phenotype: Name of phenotype to use.
+        :param mutations: Name of mutations dataset to use.
+        :param comparison: Comparison to use.  Compatible with phenotype.
         :param trials: Number of times to run the permutation test.
-        :return: New instance of PermutationTest.
-        """
-        # Load necessary data:
-        self.muts, self.phen = parse.data(phenotype_title=phenotype,
-                                          mutation_title=mutations)
-        self.sparse = parse.sparse_mutations()
-        self.sparse = parse.restrict_sparse_mutations(self.sparse, self.phen,
-                                                      self.muts)
-        self.dag = parse.dag()
-
-        # Result list for each mutual information.
-        self.results = []
-        # Dictionary of gene count -> list of mutual info's.
-        self.by_gene_count = {}
-        # The comparison function to use!
-        self.comparison = comparison
-
-        # This experiment is simply repeated for many trials.  No parameters
-        # are varying.
-        self.params['Trial'] = range(trials)
-        self.trials = trials
-
-    def run_task(self, config):
-        """
-        Task that is repeated for each trial.
-
-        Generate a random mutation dataset.  Run the dag_pattern_recover()
-        function using the random dataset, along with the original phenotype
-        and DAG.
-        :param config: Variable containing the parameters (in this case,
-        just the trial number).
-        :return: A 2-tuple:
-        [0] Mutual information of the best root.
-        [1] Dictionary of best mutual info by number of genes in subtree.
-        """
-        taskid = config[0]
-        rand_muts = randomize_mutations(self.muts, self.sparse)
-        dag_copy = self.dag.copy()
-        by_gene_count = dag_pattern_recover(rand_muts, self.phen, dag_copy,
-                                            comparison=self.comparison)
-        maxroot = get_max_root(dag_copy)
-        return dag_copy.node[maxroot]['value'], by_gene_count
-
-    def task_callback(self, retval):
-        """
-        Store the data from the task.  Executed on main process.
-        :param retval: Return value from run_task().
-        :return: None
-        """
-        mi, by_gene_count = retval
-        # Just add the best MI to the list.
-        self.results.append(mi)
-        # Append each element of the dictionary to the overall dictionary's
-        # lists.
-        for gene_count, best_mutual_info in by_gene_count.items():
-            mutual_info_list = self.by_gene_count.get(gene_count, [])
-            mutual_info_list.append(best_mutual_info)
-            self.by_gene_count[gene_count] = mutual_info_list
-
-
-class LifetimePermutationTest(Experiment):
-    """
-    Permutation Test to determine whether lifetime data produces interesting
-    splits and P-values.
-
-    This experiment does the following task many times: create a random,
-    dataset similar to the real mutations dataset.  Run dag_pattern_recover
-    using the log_rank comparison.  Look at the dag produced.  Rank the
-    terminal function nodes by their function value.  Record the top N.
-
-    The distribution of each of the N values are stored, and can be plotted
-    against the same data from the real data for comparison.
-    """
-
-    def __init__(self, phenotype='lifetime', mutations='mutations',
-                 trials=1000, ranks=200):
-        """
-        Create an instance of LifetimePermutationTest.
-        :param trials: Number of times to run the permutation test.
+        :param ranks: Number of top ranked values to return.
         :return: New instance of LifetimePermutationTest.
         """
         # Load necessary data:
@@ -213,8 +138,8 @@ class LifetimePermutationTest(Experiment):
         """
         rand_muts = randomize_mutations(self.muts, self.sparse)
         dag_copy = self.dag.copy()
-        by_gene_count = dag_pattern_recover(rand_muts, self.phen, dag_copy,
-                                            comparison=self.comparison)
+        dag_pattern_recover(rand_muts, self.phen, dag_copy,
+                            comparison=self.comparison)
 
         nodes = list(terminal_function_nodes(dag_copy))
         values = [0] * len(nodes)
@@ -222,7 +147,6 @@ class LifetimePermutationTest(Experiment):
             values[i] = dag_copy.node[node]['value']
         values.sort(reverse=True)
         return values[:self.ranks]
-
 
     def task_callback(self, retval):
         """
